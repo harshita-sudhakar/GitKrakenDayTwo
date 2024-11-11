2fbe604c5017a556295d869bc5bc9f424b8f3922
harshita-sudhakar <harshita.sudhakar@gmail.com>
2024-11-11 13:53:29 -0500

    Created The ExecutorService and Future
    Made the code count to a 1000

--- GitKrakenDay2.java
+++ GitKrakenDay2.java
@@ -1,3 +1,29 @@
-public class GitKrakenDay2 {
-    
+import java.util.concurrent.*;
+
+public class GitKrakenDay2 implements Callable<Integer> {
+
+    static int total = 0;
+    static ExecutorService executorService = Executors.newFixedThreadPool(1000);
+    public static void main (String [] args) {
+        Future <Integer> future = executorService.submit(new GitKrakenDay2());
+        for (int i = 0; i < 1000; i++) {
+            try {
+                total += future.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("the total is: " + total);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int total = 0;
+        for (int i = 0; i < 1000000; i++) {
+            total++;
+        }
+        return total;
+    }
 }
